7d0bd3d6f6b9e6a8b98c99fbeaaa4fe355f4142e
zh张欢 <huanzhang@trip.com>
2021-03-23 19:14:48 +0800

    tdd测试第一课

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import TodoList from './containers/TodoList';
 
 function App() {
     return (
-        <div className="app-container" title="dell lee" data-test="container">
-            <div>hello world</div>
+        <div>
+            <TodoList />
         </div>
     );
 }
---
+++ src/containers/TodoList/components/Header.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: "",
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            value: e.target.value
+        })
+    }
+
+    render() {
+        const {value} = this.state;
+        return <div>
+            <input data-test='input' value={value} onChange={this.handleInputChange.bind(this)}/>
+        </div>
+    }
+}
+
+export default Header;
